b1eb9e050432da80f26754900d7401780c7c74b8
daniel_fillol@hotmail.com <55287657+DanielFillol@users.noreply.github.com>
2023-10-19 20:34:43 -0300

    feat: function to get the end of the URL using the decomposed.TJ from github.com/DanielFillol/CNJ_Validate/CNJ

---
+++ request/api_define_tj.go
@@ -0,0 +1,16 @@
+package request
+
+import (
+	"github.com/DanielFillol/CNJ_Validate/CNJ"
+	"strings"
+)
+
+func defineTJ(cnjNumber string) (string, error) {
+	CNJ.ValidateCNJ()
+	decomposedCNJ, err := CNJ.DecomposeCNJ(cnjNumber)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.ToLower(decomposedCNJ.TJ), nil
+}
