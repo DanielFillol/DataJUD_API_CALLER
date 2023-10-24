537cfd4d485d6338b9a797749f0bcbd4c9fb292c
daniel_fillol@hotmail.com <55287657+DanielFillol@users.noreply.github.com>
2023-10-24 01:16:48 -0300

    feat: create reading pkg for CNJ code search with pagination feature of the API

---
+++ csv/read_codeSearch.go
@@ -0,0 +1,50 @@
+package csv
+
+import (
+	"encoding/csv"
+	"github.com/DanielFillol/DataJUD_API_CALLER/models"
+	"log"
+	"os"
+)
+
+// The ReadCode function reads data from a CSV file located at the specified filePath, with the specified separator.
+// It returns a slice of models.ReadCsvCode structs containing the data from the CSV file, excluding the header.
+func ReadCode(filePath string, separator rune, skipHeaderLine bool) ([]models.ReadCsvCode, error) {
+	csvFile, err := os.Open(filePath)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer csvFile.Close()
+
+	csvR := csv.NewReader(csvFile)
+	csvR.Comma = separator
+
+	csvData, err := csvR.ReadAll()
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	var data []models.ReadCsvCode
+	for i, line := range csvData {
+		if skipHeaderLine {
+			// Skip the header line
+			if i != 0 {
+				data = append(data, models.ReadCsvCode{
+					ClassCode: line[0],
+					CourtCode: line[1],
+					CourtName: line[2],
+				})
+			}
+		} else {
+			data = append(data, models.ReadCsvCode{
+				ClassCode: line[0],
+				CourtCode: line[1],
+				CourtName: line[2],
+			})
+		}
+	}
+
+	return data, nil
+}
